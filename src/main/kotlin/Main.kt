04d02cc533165b69a8c28fdf2b45888dab613f2d
MCprotein <chss3339@gmail.com>
2023-08-14 20:54:39 +0900

    변수

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val s = "ABC"
+    val i = 1
+    val l = 1L
+    val d = 1.0
+    val f = 1.0f
+
+    println("s = ${s::class}")
+    println("i = ${i::class}")
+    println("l = ${l::class}")
+    println("d = ${d::class}")
+    println("f = ${f::class}")
 }
